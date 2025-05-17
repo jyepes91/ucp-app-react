97928d81452a791eb126cc22b1a768c15edf8eb3
jyepes <jorgemarioey91@gmail.com>
2025-05-17 16:59:50 -0500

    agregar mi nombre

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div>
       <h1>¡Universidad Católica de Pereira !</h1>
       <p>Listado de integrantes - Proceso de Desarrollo de Software I</p>
+      <h1>Estudiante:Jorge Mario Echeverri Yepes</h1>
     </div>
   );
 }
