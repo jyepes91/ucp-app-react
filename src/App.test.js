bce35d398cf4e38003c4fdf50002bfb333b9205e
jyepes <jorgemarioey91@gmail.com>
2025-05-24 11:57:21 -0500

    testes

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Estudiante:Jorge Mario Echeverri Yepes/i);
   expect(linkElement).toBeInTheDocument();
 });
